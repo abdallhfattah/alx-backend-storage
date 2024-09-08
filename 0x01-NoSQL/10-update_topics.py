051721ce0d5e1555347a2ebb38d8935ded2808ba
abdallhfattah <abdallh.fattah33@gmail.com>
2024-09-08 21:20:25 +0300

    update

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,4 +4,4 @@
 
 def update_topics(mongo_collection, name, topics):
     """updating topics using name"""
-    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
