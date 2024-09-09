32f2d6571c1f96cacf65d8f328b84a83708c3dc2
abdallhfattah <abdallh.fattah33@gmail.com>
2024-09-09 21:41:07 +0300

    caching request for 10 seconds

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,28 +9,28 @@ from typing import Callable
 
 
 def track_get_page(fn: Callable) -> Callable:
-    """ Decorator for get_page
-    """
+    """Decorator for get_page"""
+
     @wraps(fn)
     def wrapper(url: str) -> str:
-        """ Wrapper that:
-            - check whether a url's data is cached
-            - tracks how many times get_page is called
+        """Wrapper that:
+        - check whether a url's data is cached
+        - tracks how many times get_page is called
         """
         client = redis.Redis()
-        client.incr(f'count:{url}')
-        cached_page = client.get(f'{url}')
+        client.incr(f"count:{url}")
+        cached_page = client.get(f"{url}")
         if cached_page:
-            return cached_page.decode('utf-8')
+            return cached_page.decode("utf-8")
         response = fn(url)
-        client.set(f'{url}', response, 10)
+        client.set(f"{url}", response, 10)
         return response
+
     return wrapper
 
 
 @track_get_page
 def get_page(url: str) -> str:
-    """ Makes a http request to a given endpoint
-    """
+    """Makes a http request to a given endpoint"""
     response = requests.get(url)
     return response.text
