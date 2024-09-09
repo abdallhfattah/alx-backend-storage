d8f5b456db1c9616d023d7186d3a7779e10d840b
abdallhfattah <abdallh.fattah33@gmail.com>
2024-09-09 13:52:02 +0300

    get with conversions

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -13,6 +13,18 @@ class Cache:
         self._redis.flushdb(True)
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        key = str(uuid.uuid1())
+        key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str, fn: callable):
+        obj = self._redis.get(key)
+        if obj is None:
+            return None
+        return fn(obj) if fn else obj
+
+    def get_str(self, key: str) -> str:
+        return self.get(key, lambda x: x.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        return self.get(key, int)
